ac5b608b96a91afa1aab0711c360fa4283f9cfae
Unknown <zebahashimi@gmail.com>
2018-05-03 15:50:54 -0700

    Added Comments in Code

--- Collector/AddItemViewController.swift
+++ Collector/AddItemViewController.swift
@@ -18,6 +18,7 @@ class AddItemViewController: UIViewController, UIImagePickerControllerDelegate, 
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    //code allowing the display of photo that was picked
         imagePicker.delegate = self
     }
     // code for pulling up photos from your photo library
@@ -25,12 +26,12 @@ class AddItemViewController: UIViewController, UIImagePickerControllerDelegate, 
         imagePicker.sourceType = .photoLibrary
         present(imagePicker, animated: true, completion: nil)
     }
-    
+    // code for taking photos on camera
     @IBAction func cameraTapped(_ sender: Any) {
         imagePicker.sourceType = .camera
         present(imagePicker, animated: true, completion: nil)
     }
-    
+    //code allowwing user to select photo
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         
         if let choosenImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
@@ -41,6 +42,7 @@ class AddItemViewController: UIViewController, UIImagePickerControllerDelegate, 
     }
     
     // this piece of code below will allow us to push data (photo and title that user will enter into app)to core data so we can save it. This is why we need to first add the entity, then write code for the elements inside our entity which is Title and Image. Now that we have passed Title and Image to Core Data, we want to save it and then push user back to the main screen. Once on the main screen, we need to write more code for the user to be able to access the photo and title that was added.
+  
     @IBAction func addTapped(_ sender: Any) {
         
         if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
